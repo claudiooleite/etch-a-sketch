8795225d15e41d9e214b57213ac3979363179fef
claudiooleite <152962012+claudiooleite@users.noreply.github.com>
2024-03-04 15:53:12 +0100

    mousedown function added and working

--- javascript.js
+++ javascript.js
@@ -1,34 +1,45 @@
-// changing color
-let box = document.querySelector('#box');
-let newDiv = box.querySelectorAll('.newDiv')
 
-newDiv.forEach(newDiv => {
-    newDiv.addEventListener('click', function (e) {
-        e.target.style.background = 'black';
-    });
+
+let box = document.querySelector('#box');
+let mouseDown = false;
+
+// Event delegation 
+
+box.addEventListener('mousedown', function (e) {
+    mouseDown = true;
+
+    if (e.target.classList.contains('newDiv')) {
+        e.target.style.backgroundColor = 'black';
+    }
 });
 
+box.addEventListener('mouseup', function () {
+    mouseDown = false;
+});
+
+box.addEventListener('mouseover', function (e) {
+    if (mouseDown && e.target.classList.contains('newDiv')) {
+        e.target.style.backgroundColor = 'black';
+    }
+});
+
+
 // changing box size
+function createDiv(size) {
+    box.style.gridTemplateColumns = `repeat(${size / 2}, 1fr)`;
+    box.style.gridTemplateRows = `repeat(${size / 2}, 1fr)`;
 
-function createDiv(size){
-    box.style.gridTemplateColumns = `repeat(${size/4}, 1fr)`;
-    box.style.gridTemplateRows = `repeat(${size/4}, 1fr)`;
-
-    for(let i = 1; i <= size; i++){
-        newDiv = document.createElement('div');
+    // Clear existing content
+    box.innerHTML = '';
+    let resized = size **2;
+    // Add new divs with the class 'newDiv'
+    for (let i = 1; i <= resized; i++) {
+        const newDiv = document.createElement('div');
         newDiv.className = 'newDiv';
         box.appendChild(newDiv);
-        newDiv.forEach(newDiv => {
-            newDiv.addEventListener('click', function (e) {
-                e.target.style.background = 'black';
-            });
-        });
     }
-};
+}
 
-
-createDiv(16);
-
-
-
+// Example usage with size 16
+createDiv(100);
 
