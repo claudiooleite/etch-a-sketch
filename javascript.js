566282d60272595e37298464f86c6c5b59c3aae1
claudiooleite <152962012+claudiooleite@users.noreply.github.com>
2024-03-04 13:21:17 +0100

    div box sized and on click working

--- javascript.js
+++ javascript.js
@@ -1,7 +1,6 @@
-
-const box = document.querySelector('#box');
-
-const newDiv = box.querySelectorAll('.newDiv')
+// changing color
+let box = document.querySelector('#box');
+let newDiv = box.querySelectorAll('.newDiv')
 
 newDiv.forEach(newDiv => {
     newDiv.addEventListener('click', function (e) {
@@ -9,17 +8,27 @@ newDiv.forEach(newDiv => {
     });
 });
 
+// changing box size
+
+function createDiv(size){
+    box.style.gridTemplateColumns = `repeat(${size/4}, 1fr)`;
+    box.style.gridTemplateRows = `repeat(${size/4}, 1fr)`;
+
+    for(let i = 1; i <= size; i++){
+        newDiv = document.createElement('div');
+        newDiv.className = 'newDiv';
+        box.appendChild(newDiv);
+        newDiv.forEach(newDiv => {
+            newDiv.addEventListener('click', function (e) {
+                e.target.style.background = 'black';
+            });
+        });
+    }
+};
 
 
-// function createDiv(size){
-//     for(let i = 1; i <= size; i++){
-//         newDiv = document.createElement('div');
-//         newDiv.className = 'newDiv';
-//         newDiv.style.cssText = `background-Color: black; grid-template-columns: repeat(${size}, 1fr); grid-template-rows: repeat(${size}, 1fr)`;
-//         box.appendChild(newDiv);
-//     }
-// }
+createDiv(16);
 
-// let sizing = createDiv(16);
+
 
 
